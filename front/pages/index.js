1dd5a24b90e46e284f1d48e6600e13f87c10b7be
Kang <rkdguswns0803@gmail.com>
2022-03-02 22:42:43 +0900

    Page와 Layout

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
   return (
-    <div>Hello, Next!</div>
+    <AppLayout>
+      {/* AppLayout로 감싸진 태그들이 children이 된다 */}
+      <div>Hello, Next!</div>
+    </AppLayout>
   );
-}
+};
 
 export default Home;
