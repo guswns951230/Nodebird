ae89ce45efa73b538296c10a1234da50e1bc2d4b
Kang <rkdguswns0803@gmail.com>
2022-03-03 19:10:25 +0900

    antd와 styled-components

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { Menu } from 'antd';
 
 const AppLayout = ({ children }) => { // pages의 파일들이  공통적으로 사용할 Layout
   return (
     <div>
-      <div>
-        <Link href="/"><a>NodeBird</a></Link>
-        <Link href="/profile"><a>My Profile</a></Link>
-        <Link href="/signup"><a>Sign Up</a></Link>
-      </div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/"><a>NodeBird</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile"><a>My Profile</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup"><a>Sign Up</a></Link>
+        </Menu.Item>
+      </Menu>
       {children}
     </div>
   );
