caddf2dcd4673ed5d44b044de1df65aa723ea251
Kang <rkdguswns0803@gmail.com>
2022-03-03 19:29:13 +0900

    _app.js와 Head

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
 const Profile = () => {
   return (
-    <AppLayout>
-      <div>My Profile Page</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>My Profile | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>My Profile Page</div>
+      </AppLayout>
+    </>
   );
 };
 
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
 const SignUp = () => {
   return (
-    <AppLayout>
-      <div>Sign Up Page</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>Sign Up | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>Sign Up Page</div>
+      </AppLayout>
+    </>
   );
 };
 
