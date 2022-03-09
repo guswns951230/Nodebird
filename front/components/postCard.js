273afd261b79ed829d8d94eae927fc7c062dc160
Kang <rkdguswns0803@gmail.com>
2022-03-09 16:59:59 +0900

    댓글 구현하기

--- front/components/postCard.js
+++ front/components/postCard.js
@@ -1,10 +1,11 @@
 import React, { useState, useCallback } from "react";
 import PropTypes from 'prop-types';
 import { useSelector } from "react-redux";
-import { Button, Card, Popover, Avatar } from 'antd';
+import { Button, Card, Popover, Avatar, List, Comment } from 'antd';
 import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, HeartTwoTone } from '@ant-design/icons';
 
 import PostImages from "./PostImages";
+import CommentForm from "./CommentForm";
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -52,15 +53,28 @@ const PostCard = ({ post }) => {
       </Card>
       {commentFormOpened && (
         <div>
-          댓글부분
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
         </div>)}
-      {/* <CommentForm />
-      <Comments /> */}
+
     </div >
   );
 };
 
-PostCard.PropTypes = {
+PostCard.propTypes = {
   post: PropTypes.shape({
     id: PropTypes.number,
     User: PropTypes.object,
