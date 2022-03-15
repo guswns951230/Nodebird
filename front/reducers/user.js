634ef13b23ed1692944c191b4cf9ee7c62e81f8c
Kang <rkdguswns0803@gmail.com>
2022-03-15 22:14:03 +0900

    Follow Unfollow 구현하기

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,18 +1,30 @@
 import produce from "immer";
 
 export const initialState = {
+  followLoading: false,  // follow 시도중
+  followDone: false,
+  followError: null,
+
+  unfollowLoading: false,  // unfollow 시도중
+  unfollowDone: false,
+  unfollowError: null,
+
   logInLoading: false, // login 시도중
   logInDone: false,
   logInError: null,
+
   logOutLoading: false,  // logout 시도중
   logOutDone: false,
-  logOutError: false,
+  logOutError: null,
+
   signUpLoading: false,  // signup 시도중
   signUpDone: false,
-  signUpError: false,
+  signUpError: null,
+
   chageNicknameLoading: false,  // nickname 변경 시도중
   chageNicknameDone: false,
-  chageNicknameError: false,
+  chageNicknameError: null,
+
   me: null,
   signUpData: {},
   loginData: {},
@@ -72,6 +84,36 @@ export const logoutRequestAction = (data) => {
 const reducer = (state = initialState, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
+      case FOLLOW_REQUEST:
+        draft.followLoading = true;
+        draft.followDone = false;
+        draft.followError = null;
+        break;
+      case FOLLOW_SUCCESS:
+        draft.followLoading = false;
+        draft.followDone = true;
+        draft.me.Followings.push({ id: action.data });
+        break;
+      case FOLLOW_FAILURE:
+        draft.followLoading = false;
+        draft.followError = action.error;
+        break;
+
+      case UNFOLLOW_REQUEST:
+        draft.unfollowLoading = true;
+        draft.unfollowDone = false;
+        draft.unfollowError = null;
+        break;
+      case UNFOLLOW_SUCCESS:
+        draft.unfollowLoading = false;
+        draft.unfollowDone = true;
+        draft.me.Followings = draft.me.Followings.filter((v) => v.id !== action.data);
+        break;
+      case UNFOLLOW_FAILURE:
+        draft.unfollowLoading = false;
+        draft.unfollowError = action.error;
+        break;
+
       case 'LOG_IN_REQUEST':
         console.log('reducer logIn');
         draft.logInLoading = true;
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,15 +1,54 @@
 import axios from 'axios';
 import { all, fork, delay, put, takeLatest } from 'redux-saga/effects';
 import {
+  FOLLOW_REQUEST, FOLLOW_SUCCESS, FOLLOW_FAILURE,
+  UNFOLLOW_REQUEST, UNFOLLOW_SUCCESS, UNFOLLOW_FAILURE,
   LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE,
   LOG_OUT_REQUEST, LOG_OUT_SUCCESS, LOG_OUT_FAILURE,
   SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
 } from '../reducers/user';
 
+function followAPI() { // * 붙이면 error
+  return axios.post('/api/follow');
+}
+function* follow(action) {
+  try {
+    // const result = yield call(followAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: FOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    yield put({
+      type: FOLLOW_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function unfollowAPI() { // * 붙이면 error
+  return axios.post('/api/unfollow');
+}
+function* unfollow(action) {
+  try {
+    // const result = yield call(unfollowAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: UNFOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    yield put({
+      type: UNFOLLOW_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
 function logInAPI(data) { // * 붙이면 error
   return axios.post('/api/login', data);
 }
-
 function* logIn(action) {
   try {
     console.log('saga logIn');
@@ -30,14 +69,12 @@ function* logIn(action) {
 function logOutAPI() { // * 붙이면 error
   return axios.post('/api/logout');
 }
-
 function* logOut() {
   try {
-    // const result = yield call(logOutAPI);  // call은 logInAPI가 return할때까지 기다렸다 result에 넣음
+    // const result = yield call(logOutAPI, action.data);
     yield delay(1000);
     yield put({
       type: LOG_OUT_SUCCESS,
-      // data: result.data,
     });
   } catch (err) {
     yield put({
@@ -50,14 +87,12 @@ function* logOut() {
 function signUpAPI() { // * 붙이면 error
   return axios.post('/api/logout');
 }
-
 function* signUp() {
   try {
-    // const result = yield call(signUpAPI);  // call은 logInAPI가 return할때까지 기다렸다 result에 넣음
+    // const result = yield call(signUpAPI, action.data);
     yield delay(1000);
     yield put({
       type: SIGN_UP_SUCCESS,
-      // data: result.data,
     });
   } catch (err) {
     yield put({
@@ -65,6 +100,14 @@ function* signUp() {
       error: err.response.data,
     });
   }
+}
+
+function* watchFollow() {
+  yield takeLatest(FOLLOW_REQUEST, follow);
+}
+
+function* watchUnfollow() {
+  yield takeLatest(UNFOLLOW_REQUEST, unfollow);
 }
 
 function* watchLogIn() {
@@ -81,6 +124,8 @@ function* watchSignUp() {
 
 export default function* userSaga() {
   yield all([
+    fork(watchFollow),
+    fork(watchUnfollow),
     fork(watchLogIn),
     fork(watchLogOut),
     fork(watchSignUp),
