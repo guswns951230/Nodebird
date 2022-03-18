1442cf463b22fe8bd73875cb4dc83941e98b79c9
Kang <rkdguswns0803@gmail.com>
2022-03-18 17:09:33 +0900

    middleware로 router 검사하기

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 const { User, Post } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const db = require('../models');
 
 const router = express.Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
       console.error(err);
@@ -41,7 +42,7 @@ router.post('/login', (req, res, next) => {
   })(req, res, next); // middleware 확장
 });
 
-router.post('/', async (req, res, next) => {  // POST /user/
+router.post('/', isNotLoggedIn, async (req, res, next) => {  // POST /user/
   try {
     const exUser = await User.findOne({
       where: {
@@ -64,7 +65,7 @@ router.post('/', async (req, res, next) => {  // POST /user/
   }
 });
 
-router.post('/user/logout', (req, res) => {
+router.post('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
   res.send('ok');
