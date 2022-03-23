569c2539e429d46d781f7b489ad58771ef3a1bde
Kang <rkdguswns0803@gmail.com>
2022-03-24 03:22:41 +0900

    Hashtag 등록하기

--- back/routes/post.js
+++ back/routes/post.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const { Post, User, Comment, Image } = require('../models');
+const { Post, User, Comment, Image, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 const image = require('../models/image');
 
@@ -32,10 +32,17 @@ const upload = multer({
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {  // Post /post
   try {
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
     const post = await Post.create({
       content: req.body.content,
       UserId: req.user.id,
     });
+    if (hashtags) {
+      const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({
+        where: { name: tag.slice(1).toLowerCase() },
+      }))); // [[노드, true], [리액트, true]]
+      await post.addHashtags(result.map((v) => v[0]));
+    }
     if (req.body.image) {
       if (Array.isArray(req.body.image)) {  // 이미지를 여러 개 올리면 image: [이미지1.png, 이미지2.png] 배열형태
         const images = await Promise.all(req.body.image.map((image) => Image.create({ src: image })));
